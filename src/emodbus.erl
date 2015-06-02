8f4734f3e6971330cf5fab0b5d7a9e35e745c3f7
Feng Lee <ery.lee@gmail.com>
2015-06-02 22:10:06 +0800

    response

--- src/emodbus.erl
+++ src/emodbus.erl
@@ -84,28 +84,28 @@ disconnect(Device) ->
     call(Device, stop).
 
 read_coils(Device, Offset, Count) ->
-    call(Device, {?FUN_CODE_READ_COILS, {Offset, Count}}).
+    call(Device, #modbus_req{funcode = ?FUN_CODE_WRITE_COILS, offset = Offset, count = Count}).
 
 read_inputs(Device, Offset, Count) ->
-    call(Device, {?FUN_CODE_READ_INPUTS, {Offset, Count}}).
+    call(Device, #modbus_req{funcode = ?FUN_CODE_READ_INPUTS, offset = Offset, count = Count}).
 
 read_hregs(Device, Offset, Count) ->
-    call(Device, {?FUN_CODE_READ_HREGS, {Offset, Count}}).
+    call(Device, #modbus_req{funcode = ?FUN_CODE_READ_HREGS, offset = Offset, count = Count}).
 
 read_iregs(Device, Offset, Count) ->
-    call(Device, {?FUN_CODE_READ_IREGS, {Offset, Count}}).
+    call(Device, #modbus_req{funcode = ?FUN_CODE_READ_IREGS, offset = Offset, count = Count}).
 
-write_coil(Device, Offset, Coil) ->
-    call(Device, {?FUN_CODE_WRITE_COIL, {Offset, Coil}}).
+write_coil(Device, Offset, Coil) when Coil =:= 0 orelse Coil =:= 1 ->
+    call(Device, #modbus_req{funcode = ?FUN_CODE_WRITE_COIL, offset = Offset, count = onoff(Coil)}).
 
-write_hreg(Device, Offset, Value) ->
-    call(Device, {?FUN_CODE_WRITE_HREG, {Offset, Value}}).
+write_hreg(Device, Offset, Value) when Value >= 0 andalso Value =< 16#FFFF ->
+    call(Device, #modbus_req{funcode = ?FUN_CODE_WRITE_HREG, offset = Offset, count = Value}).
 
-write_coils(Device, Offset, Coils) ->
-    call(Device, {?FUN_CODE_WRITE_COILS, {Offset, Coils}}).
+write_coils(_Device, _Offset, _Coils) ->
+    {error, unsupport}.
 
-write_hregs(Device, Offset, Value) ->
-    call(Device, {?FUN_CODE_WRITE_HREGS, {Offset, Value}}).
+write_hregs(_Device, _Offset, _Value) ->
+    {error, unsupport}.
 
 call(Device, Request) ->
     gen_server:call(Device, Request, ?CALL_TIMEOUT).
@@ -118,8 +118,8 @@ init([Host, Port, UnitId]) ->
             {stop, {error, Error}}
     end.
 
-handle_call({FunCode, Params}, _From, State) ->
-    case catch request(FunCode, Params, State) of
+handle_call(Req, _From, State) when is_record(Req, modbus_req) ->
+    case catch request(Req, State) of
         {'EXIT', Error} ->
             {reply, {error, Error}, next_id(State)};
         Response ->
@@ -130,7 +130,7 @@ handle_call(stop, _From, State=#state{socket = Sock}) ->
 	gen_tcp:close(Sock),
 	{stop, normal, stopped, State};
 
-handle_call(_Request, _From, State) ->
+handle_call(_Req, _From, State) ->
     {reply, {error, badreq}, State}.
 
 handle_cast(_Msg, State) ->
@@ -145,39 +145,15 @@ terminate(_Reason, _State) ->
 code_change(_OldVsn, State, _Extra) ->
     {ok, State}.
 
-request(?FUN_CODE_READ_COILS, {Offset, Count}, State) ->
-    send_and_recv(?FUN_CODE_READ_COILS, <<Offset:16, Count:16>>, State);
+request(Req, State) ->
+    send_and_recv(Req, State).
 
-request(?FUN_CODE_READ_INPUTS, {Offset, Count}, State) ->
-    send_and_recv(?FUN_CODE_READ_INPUTS, <<Offset:16, Count:16>>, State);
-
-request(?FUN_CODE_READ_HREGS, {Offset, Count}, State) ->
-    send_and_recv(?FUN_CODE_READ_HREGS, <<Offset:16, Count:16>>, State);
-
-request(?FUN_CODE_READ_IREGS, {Offset, Count}, State) ->
-    send_and_recv(?FUN_CODE_READ_IREGS, <<Offset:16, Count:16>>, State);
-
-request(?FUN_CODE_WRITE_COIL, {Offset, Coil}, State) ->
-    send_and_recv(?FUN_CODE_WRITE_COIL, <<Offset:16, Coil:16>>, State);
-
-request(?FUN_CODE_WRITE_HREG, {Offset, Value}, State) ->
-    send_and_recv(?FUN_CODE_WRITE_HREG, <<Offset:16, Value:16>>, State);
-
-request(?FUN_CODE_WRITE_COILS, {Offset, Coils}, State) ->
-    {error, unsupported};
-
-request(?FUN_CODE_WRITE_HREGS, {Offset, Values}, State) ->
-    {error, unsupported};
-
-request(_FunCode, _Params, _State) ->
-    {error, unsupported}.
-
-send_and_recv(FunCode, Payload, State) ->
-    case send(FunCode, Payload, State) of
+send_and_recv(Req, State) ->
+    case send(Req, State) of
         ok ->
-            case recv(FunCode, State) of
+            case recv(State) of
                 {ok, Frame} ->
-                    response(FunCode, Frame);
+                    response(Req, Frame);
                 {error, Reason} ->
                     {error, Reason}
             end;
@@ -185,26 +161,34 @@ send_and_recv(FunCode, Payload, State) ->
             {error, Reason}
     end.
 
-response(?FUN_CODE_READ_COILS, Frame) ->
-    ok;
-
-response(?FUN_CODE_READ_COILS, Frame) ->
-    ok.
-
-response(?FUN_CODE_READ_INPUTS, Frame) ->
+response(#modbus_req{funcode = FunCode, count = Count}, #modbus_frame{funcode = FunCode, payload = Payload}) ->
+    case FunCode of
+        ?FUN_CODE_READ_COILS -> 
+            <<_:8, Data/binary>> = Payload,
+            {ok, bits(Data, Count, [])};
+        ?FUN_CODE_READ_INPUTS ->
+            <<_:8, Data/binary>> = Payload,
+            {ok, bits(Data, Count, [])};
+        ?FUN_CODE_READ_HREGS ->
+            <<_:8, Data/binary>> = Payload,
+            {ok, words(Data)};
+        ?FUN_CODE_READ_IREGS ->
+            <<_:8, Data/binary>> = Payload,
+            {ok, words(Data)};
+        ?FUN_CODE_WRITE_COIL ->
+            <<_:16, Coil:16>> = Payload,
+            {ok, onoff(Coil)};
+        ?FUN_CODE_WRITE_HREG ->
+            <<_:16, Val:16>> = Payload,
+            {ok, Val}
+    end;
     
-response(?FUN_CODE_READ_HREGS, Frame) ->
+response(#modbus_req{funcode = FunCode}, #modbus_frame{funcode = ErroCode, payload = Payload}) 
+    when ErroCode == (FunCode + 16#80) -> {error, err(Payload)}.
 
-response(?FUN_CODE_READ_IREGS, Frame) ->
-
-response(?FUN_CODE_WRITE_COIL, Frame) ->
-
-response(?FUN_CODE_WRITE_HREG, Frame) ->
-
-
-send(FunCode, Payload, #state{tid = Tid, unit_id = UnitId, socket = Sock}) ->
+send(#modbus_req{funcode = FunCode, offset = Offset, count = Count}, #state{tid = Tid, unit_id = UnitId, socket = Sock}) ->
     Hdr= #mbap_header{tid = Tid, unit_id = UnitId},
-    Frame = #modbus_frame{header = Hdr, funcode = FunCode, payload = Payload},
+    Frame = #modbus_frame{header = Hdr, funcode = FunCode, payload = <<Offset:16, Count:16>>},
     gen_tcp:send(Sock, serialize(Frame)).
 
 serialize(#modbus_frame{header = #mbap_header{tid = Tid, unit_id = Uid},
@@ -212,15 +196,15 @@ serialize(#modbus_frame{header = #mbap_header{tid = Tid, unit_id = Uid},
     Len = 2 + size(Payload),
     <<Tid:16, 0:16, Len:16, Uid:8, FunCode:8, Payload/binary>>.
     
-recv(FunCode, State = #state{tid = Tid, socket = Sock}) ->
-    case recv(header, FunCode, State) of
+recv(State) ->
+    case recv(header, State) of
         {ok, Header} ->
             recv(payload, Header, State);
         {error,Error} ->
             {error,Error}
     end.
 
-recv(header, _FunCode, State = #state{tid = Tid, socket = Sock}) ->
+recv(header, State = #state{tid = Tid, socket = Sock}) ->
     case gen_tcp:recv(Sock, ?MBAP_LENGTH, ?RECV_TIMEOUT) of
         {ok, <<Tid:16, 0:16, Len:16, Uid:8>>} ->
             recv(payload, #mbap_header{tid = Tid, length = Len, unit_id = Uid}, State);
@@ -229,7 +213,7 @@ recv(header, _FunCode, State = #state{tid = Tid, socket = Sock}) ->
             {error, badresp};
         {error, Reason} ->
             {error, Reason}
-    end;
+    end.
 
 recv(payload, Header = #mbap_header{length = Len}, #state{socket = Sock}) ->
     case gen_tcp:recv(Sock, Len - 1, ?RECV_TIMEOUT) of
@@ -244,4 +228,33 @@ next_id(State = #state{tid = Tid}) when Tid >= 16#EFFF ->
 
 next_id(State = #state{tid = Tid}) ->
     State#state{tid = Tid+1}.
+
+onoff(1) -> 16#FF00;
+onoff(16#FF00) -> 1;
+onoff(_) -> 0.
+
+bits(_Data, 0, Acc) ->
+    lists:reverse(Acc);
+bits(Data, Count, Acc) ->
+    <<Bit:1, Left/binary>> = Data,
+    bits(Left, Count-1, [Bit|Acc]).
+
+words(Data) ->
+    words(Data, []).
+words(<<>>, Acc) ->
+    lists:reverse(Acc);
+words(<<I:16, Left/binary>>, Acc) ->
+    words(Left, [I|Acc]).
+
+err(<<Code:8>>) -> err(Code); 
+err(1)  -> illegal_function;
+err(2)  -> illegal_data_address;
+err(3)  -> illegal_data_value;
+err(4)  -> slave_device_failure;
+err(5)  -> acknowledge;
+err(6)  -> slave_device_busy;
+err(8)  -> memory_parity_error;
+err(10) -> gateway_path_unavailable;
+err(11) -> gateway_target_device_failed_to_respond;
+err(_)  -> unknown_response_code.
 
