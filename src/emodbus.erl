dbaa15d2fb33b42016bf9520c7a153708cf70aae
Feng Lee <ery.lee@gmail.com>
2015-06-02 11:06:00 +0800

    fix api

--- src/emodbus.erl
+++ src/emodbus.erl
@@ -29,9 +29,8 @@
 -include("emodbus.hrl").
 
 %% API Exports
--export([start/0, start/1,
-         start_link/0, start_link/1,
-         stop/1]).
+-export([connect/0, connect/2, connect/3,
+         disconnect/1]).
 
 %% modbus API Exports
 -export([read_coils/3,
@@ -48,6 +47,12 @@
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
          terminate/2, code_change/3]).
 
+-record(state, {
+        socket      :: inet:socket(),
+        tid = 1     :: 1..16#ff,
+        unit_id = 1 :: byte()
+ }).
+
 -define(MBAP_LENGTH, 7).
 
 -define(CALL_TIMEOUT, 60000).
@@ -59,39 +64,24 @@
 %% Modbus Socket Options
 -define(SOCKOPTS, [
         binary,
-        {packet,    raw},
+        {active, false},
+        {packet, 0},
         {reuseaddr, true},
         {nodelay,   true}
 ]).
 
--type option() :: {host, inet:ip_address() | string()}
-                | {port, inet:port_number()}
-                | {logger, atom() | {atom(), atom()}
-                | {unit_id, byte()}}.
 
--record(?MODULE, {name                :: atom(),
-                  host = "localhost"  :: inet:ip_address() | string(),
-                  port = ?MODBUS_PORT :: inet:port_number(),
-                  socket              :: inet:socket(),
-                  timeout             :: pos_integer(),
-                  tid = 1             :: 1..16#ff,
-                  unit_id = 1         :: byte(),
-                  parse_state,
-                  logger              :: gen_logger:logmod()}).
+connect() ->
+    connect("localhost", ?MODBUS_PORT).
 
-start() ->
-    start([]).
+connect(Host, Port) ->
+    connect(Host, Port, 1).
 
--spec start(list(option())) -> {ok, pid()} | {error, any()}.
-start(Opts) ->
-    gen_server:start(?MODBUS_PORT, [Opts], []).
+connect(Host, Port, UnitId) ->
+    gen_server:start_link(?MODULE, [Host, Port, UnitId], []).
 
-start_link() ->
-    start_link([]).
-
--spec start_link(list(option())) -> {ok, pid()} | {error, any()}.
-start_link(Opts) ->
-    gen_server:start_link(?MODULE, [Opts], []).
+disconnect(Device) ->
+    call(Device, stop).
 
 read_coils(Device, Offset, Count) ->
     call(Device, {?FUN_CODE_READ_COILS, {Offset, Count}}).
@@ -114,58 +104,30 @@ write_coils(Device, Offset, Coils) ->
 write_hregs(Device, Offset, Value) ->
     call(Device, {?FUN_CODE_WRITE_HREGS, {Offset, Value}}).
 
-stop(Device) ->
-    call(Device, stop).
-
 call(Device, Request) ->
     gen_server:call(Device, Request, ?CALL_TIMEOUT).
 
-init([Opts]) ->
-    Logger = gen_logger:new(proplists:get_value(logger, Opts, {console, debug})),
-
-    Opts1 = proplists:delete(logger, Opts),
-
-    State = init(Opts1, #?MODULE{host    = "localhost",
-                                 port    = ?MODBUS_PORT,
-                                 logger  = Logger,
-                                 unit_id = 1}),
-
-    case connect(State) of
+init([Host, Port, UnitId]) ->
+    case gen_tcp:connect(Host, Port, ?SOCKOPTS, ?SOCK_TIMEOUT) of
         {ok, Sock} -> 
-            {ok, State#?MODULE{socket = Sock}};
-        {error, Error} ->
+            {ok, #state{socket = Sock, unit_id = UnitId}};
+        {error, Error} -> 
             {stop, {error, Error}}
     end.
 
-init([], State) ->
-    State;
-init([{host, Host} | Opts], State) ->
-    init(Opts, State#?MODULE{host = Host});
-init([{port, Port} | Opts], State) ->
-    init(Opts, State#?MODULE{port = Port});
-init([{logger, Cfg} | Opts], State) ->
-    init(Opts, State#?MODULE{logger = gen_logger:new(Cfg)});
-init([{unit_id, Id} | Opts], State) ->
-    init(Opts, State#?MODULE{unit_id = Id});
-init([_Opt | Opts], State) ->
-    init(Opts, State).
-
-connect(#?MODULE{host = Host, port = Port, timeout = Timeout}) ->
-    case gen_tcp:connect(Host, Port, ?SOCKOPTS, Timeout) of
-        {ok, Sock}     -> {ok, Sock}; 
-        {error, Error} -> {error, Error}
-    end.
-
-handle_call(Req = {FunCode, Params}, From, State) ->
-    Resp = catch request(FunCode, Params, State),
-    {reply, Resp, next_id(State)};
+handle_call({FunCode, Params}, _From, State) ->
+    Reply = case catch request(FunCode, Params, State) of
+        {'EXIT', Error} -> {error, Error};
+        Response-> Response
+    end,
+    {reply, Reply, next_id(State)};
 
 handle_call(stop, _From, State=#state{socket = Sock}) ->
 	gen_tcp:close(Sock),
 	{stop, normal, stopped, State};
 
 handle_call(_Request, _From, State) ->
-    {reply, ok, State}.
+    {reply, {error, badreq}, State}.
 
 handle_cast(_Msg, State) ->
     {noreply, State}.
@@ -180,24 +142,43 @@ code_change(_OldVsn, State, _Extra) ->
     {ok, State}.
 
 request(?FUN_CODE_READ_COILS, {Offset, Count}, State) ->
-    Payload = <<Offset:16, Count:16>>,
-    send(?FUN_CODE_READ_COILS, Payload, State),
-    case recv(?FUN_CODE_READ_COILS, State)) of
-        {ok, Frame} ->
-            response(?FUN_CODE_READ_COILS, Frame);
-        {error, Erro} ->
-            {error, Error}
-    end.
+    send_and_recv(?FUN_CODE_READ_COILS, <<Offset:16, Count:16>>, State);
 
-request(FunCode, Params, State) ->
+request(?FUN_CODE_WRITE_HREGS, {Offset, Val}, State) ->
+    send_and_recv(?FUN_CODE_WRITE_HREGS, <<Offset:16, Val:16>>, State);
+
+request(_FunCode, _Params, _State) ->
     {error, unsupported}.
 
-send(FunCode, Payload, #?MODULE{tid = Tid, unit_id = UnitId, socket = Sock}) ->
+send_and_recv(FunCode, Payload, State) ->
+    case send(FunCode, Payload, State) of
+        ok ->
+            case recv(FunCode, State) of
+                {ok, Frame} ->
+                    response(FunCode, Frame);
+                {error, Reason} ->
+                    {error, Reason}
+            end;
+        {error, Reason} ->
+            {error, Reason}
+    end.
+
+response(?FUN_CODE_READ_COILS, Frame) ->
+    ok;
+response(?FUN_CODE_READ_COILS, Frame) ->
+    ok.
+
+send(FunCode, Payload, #state{tid = Tid, unit_id = UnitId, socket = Sock}) ->
     Hdr= #mbap_header{tid = Tid, unit_id = UnitId},
     Frame = #modbus_frame{header = Hdr, funcode = FunCode, payload = Payload},
-    gen_tcp:send(Sock, emodbus_frame:serialize(Frame)).
+    gen_tcp:send(Sock, serialize(Frame)).
+
+serialize(#modbus_frame{header = #mbap_header{tid = Tid, unit_id = Uid},
+                        funcode = FunCode, payload = Payload}) ->
+    Len = 2 + size(Payload),
+    <<Tid:16, 0:16, Len:16, Uid:8, FunCode:8, Payload/binary>>.
     
-recv(FunCode, State = #?MODULE{tid = Tid, socket = Sock}) ->
+recv(FunCode, State = #state{tid = Tid, socket = Sock}) ->
     case recv(header, FunCode, State) of
         {ok, Header} ->
             recv(payload, Header, State);
@@ -205,7 +186,7 @@ recv(FunCode, State = #?MODULE{tid = Tid, socket = Sock}) ->
             {error,Error}
     end.
 
-recv(header, _FunCode, State = #?MODULE{tid = Tid, socket = Sock}) ->
+recv(header, _FunCode, State = #state{tid = Tid, socket = Sock}) ->
     case gen_tcp:recv(Sock, ?MBAP_LENGTH, ?RECV_TIMEOUT) of
         {ok, <<Tid:16, 0:16, Len:16, Uid:8>>} ->
             recv(payload, #mbap_header{tid = Tid, length = Len, unit_id = Uid}, State);
@@ -216,44 +197,13 @@ recv(header, _FunCode, State = #?MODULE{tid = Tid, socket = Sock}) ->
             {error, Reason}
     end;
 
-recv(payload, Header = #mbap_header{length = Len}, #?MODULE{socket = Sock}) ->
+recv(payload, Header = #mbap_header{length = Len}, #state{socket = Sock}) ->
     case gen_tcp:recv(Sock, Len - 1, ?RECV_TIMEOUT) of
         {ok, <<FunCodeOrErr:8, Payload/binary>>} ->
             {ok, #modbus_frame{header = Header, funcode = FunCodeOrErr, payload = Payload}};
         {error, Reason} ->
             {error, Reason}
     end.
-
-
-send(PDU, #state{tid = Tid, unit_id = Uid, socket = Sock}) ->
-
-received(<<>>, State) ->
-    {noreply, State, hibernate};
-
-received(Bytes, State = #state{parse_state = ParseState}) ->
-    case emodbus_frame:parse(Bytes, ParseState) of
-        {more, ParseState1} ->
-            {noreply, run_sock(State#state{parse_state = ParseState1}), hibernate};
-        {ok, Frame, Rest} ->
-            {ok, NewState} = handle_frame(Frame, State),
-            received(Rest, NewState#state{parse_state = emodbus_frame:init(client)});
-        {error, Error} ->
-            {stop, {shutdown, Error}, State}
-    end.
-
-handle_frame(Frame = #modbus_frame{hdr = #mbap_header{tid = Tid},
-                                   pdu = Resp}, State = #state{pending_reqs = Reqs}) ->
-    io:format("RECV Frame: ~p~n", [Frame]),
-    case gb_trees:lookup(Tid, Reqs) of
-        {value, {From, _Req}} ->
-            gen_server:reply(From, {ok, Resp#modbus_resp.bytes}),
-            {ok, State#state{pending_reqs = gb_trees:delete(Tid, Reqs)}};
-        none ->
-            {ok, State}
-    end.
-
-run_sock(State=#state{socket = Sock}) ->
-    inet:setopts(Sock, [{active, once}]), State.
 
 next_id(State = #state{tid = Tid}) when Tid >= 16#EFFF ->
     State#state{tid = 1};
